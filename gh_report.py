b973f55fa714ee233ae829d793ac0bce792314af
Jeiel Alves <jeielalves25@gmail.com>
2024-03-11 11:11:36 -0300

    generate all reports at the same time

--- gh_report.py
+++ gh_report.py
@@ -5,37 +5,34 @@ import subprocess
 import json
 import sys
 
-
-def format_month(month: int):
-    year = datetime.now().year
-
-    first_day = datetime(year, month, 1)
-    if month == 12:
-        last_day = datetime(year + 1, 1, 1) - timedelta(days=1)
-    else:
-        last_day = datetime(year, month + 1, 1) - timedelta(days=1)
-
-    start_date_str = first_day.strftime("%Y-%m-%d")
-    end_date_str = last_day.strftime("%Y-%m-%d")
-
-    return start_date_str, end_date_str
-
-
-def format_month_dot(month: int):
-    start_date_str, end_date_str = format_month(month)
-    return f"{start_date_str}..{end_date_str}"
-
-
-def format_month_since_until(month: int):
-    start_date_str, end_date_str = format_month(month)
-    return f"since={start_date_str}&until={end_date_str}"
-
-
 def month_name(month: int):
     return calendar.month_name[month]
 
+class Report:
+    def sum_defaultdict(self, d):
+        total = 0
+        for _, v in d.items():
+            total += v
+        return total
 
-class GenerateReport:
+    def one_line_result(self, d, title, file):
+        title = f"{title}\n"
+        print(title)
+        file.write(title)
+
+        for key, value in d.items():
+            key_pair = f"""{key}: {value}"""
+            file.write(key_pair + "\n")
+            print(key_pair)
+
+        total = f"TOTAL: {self.sum_defaultdict(d)}"
+        file.write(total)
+        print(total)
+        print("\n")
+        file.write("\n")
+        file.write("\n")
+
+class GenerateReport(Report):
     repo_owner: str
     repo_name: str
     date: int
@@ -85,16 +82,14 @@ class GenerateReport:
         result = subprocess.run(cmd, stdout=subprocess.PIPE)
         return json.loads(result.stdout)
 
-    def sum_by_author(self):
-        print(f"generating report from {self.date} for {self.repo_name}\n")
+    def pr_results(self):
+        prs_result = self.generate_pr_json(self.date)
 
         commits_by_author = defaultdict(int)
         deletions_by_author = defaultdict(int)
         additions_by_author = defaultdict(int)
         count_by_author = defaultdict(int)
         request_changes_by_author = defaultdict(int)
-
-        prs_result = self.generate_pr_json(self.date)
 
         total = len(prs_result)
         count = 1
@@ -119,41 +114,77 @@ class GenerateReport:
 
             count += 1
 
-        file_name = f"prs_report_{self.repo_name}_{self.repo_owner}_{self.date}.txt"
+        return {
+            "commits_by_author": commits_by_author,
+            "deletions_by_author": deletions_by_author,
+            "additions_by_author": additions_by_author,
+            "count_by_author": count_by_author,
+            "request_changes_by_author": request_changes_by_author
+        }
+
+class GenerateTotalReport(Report):
+    repo_owner: str
+    repos_name: str
+    date: int
+
+    def __init__(self, repo_owner: str, repos_name: str, date: str):
+        self.repo_owner = repo_owner
+        self.repos_name = repos_name
+        self.date = date
+
+    def sum_by_author(self):
+        repos = self.repos_name.split(":")
+
+        commits_by_author = defaultdict(int)
+        deletions_by_author = defaultdict(int)
+        additions_by_author = defaultdict(int)
+        count_by_author = defaultdict(int)
+        request_changes_by_author = defaultdict(int)
+
+        for repo in repos:
+            generate_report = GenerateReport(self.repo_owner, repo, self.date)
+            prs_result = generate_report.pr_results()
+
+            for key, value in prs_result["commits_by_author"].items():
+                commits_by_author[key] += value
+
+            for key, value in prs_result["deletions_by_author"].items():
+                deletions_by_author[key] += value
+
+            for key, value in prs_result["additions_by_author"].items():
+                additions_by_author[key] += value
+
+            for key, value in prs_result["count_by_author"].items():
+                count_by_author[key] += value
+
+            for key, value in prs_result["request_changes_by_author"].items():
+                request_changes_by_author[key] += value
+
+        return {
+            "commits_by_author": commits_by_author,
+            "deletions_by_author": deletions_by_author,
+            "additions_by_author": additions_by_author,
+            "count_by_author": count_by_author,
+            "request_changes_by_author": request_changes_by_author
+        }
+
+    def generate_file(self):
+        print(f"generating report from {self.date} for {self.repos_name}\n")
+
+        prs_result = self.sum_by_author()
+
+        file_name = f"prs_report_{self.repos_name}_{self.repo_owner}_{self.date}.txt"
         with open(file_name, "w") as file:
-            repo_name = self.repo_name.upper() + f" {self.date}" + "\n"
+            repo_name = self.repos_name.upper() + f" {self.date}" + "\n"
             print(repo_name)
             file.write(repo_name)
             file.write("\n")
 
-            self.one_line_result(count_by_author, "PR COUNT:", file)
-            self.one_line_result(commits_by_author, "COMMIT COUNT:", file)
-            self.one_line_result(additions_by_author, "LINES ADD:", file)
-            self.one_line_result(deletions_by_author, "LINES DELETED:", file)
-            self.one_line_result(request_changes_by_author, "PR CHANGE_REQUESTS:", file)
-
-    def sum_defaultdict(self, d):
-        total = 0
-        for _, v in d.items():
-            total += v
-        return total
-
-    def one_line_result(self, d, title, file):
-        title = f"{title}\n"
-        print(title)
-        file.write(title)
-
-        for key, value in d.items():
-            key_pair = f"""{key}: {value}"""
-            file.write(key_pair + "\n")
-            print(key_pair)
-
-        total = f"TOTAL: {self.sum_defaultdict(d)}"
-        file.write(total)
-        print(total)
-        print("\n")
-        file.write("\n")
-        file.write("\n")
+            self.one_line_result(prs_result["count_by_author"], "PR COUNT:", file)
+            self.one_line_result(prs_result["commits_by_author"], "COMMIT COUNT:", file)
+            self.one_line_result(prs_result["additions_by_author"], "LINES ADD:", file)
+            self.one_line_result(prs_result["deletions_by_author"], "LINES DELETED:", file)
+            self.one_line_result(prs_result["request_changes_by_author"], "PR CHANGE_REQUESTS:", file)
 
 """
     Argswith open(file_name, 'w') as file:
@@ -163,8 +194,8 @@ class GenerateReport:
         repo_name (str): The name of the repository.
         date (str): The date range which the report will be generated. Should be a date range following the example 2024-05-01..2024-05-31
 
-    example: python3 gh_report.py AGX-Software indiky-server "2024-05-01..2024-05-31"
+    example: python3 gh_report.py AGX-Software "indiky-server:core" "2024-05-01..2024-05-31"
     example 2: python3 gh_report.py AGX-Software indiky-web "2024-05-01..2024-05-31"
 """
 if __name__ == "__main__":
-    GenerateReport(*sys.argv[1:4]).sum_by_author()
+    GenerateTotalReport(*sys.argv[1:4]).generate_file()
